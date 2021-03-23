f5908e243a074e89bd5120fffd02fc442d17eb51
Abner Rodrigues <40338524+kingaspx@users.noreply.github.com>
2021-03-22 21:10:44 -0300

    Add files via upload

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -4,24 +4,24 @@ const MessageController = require('../controller/MessageController')
 const routes = new express.Router()
 
 //Sessions
-routes.get('/api/show-all-sessions', MessageController.showAllSessions)
-routes.get('/api/check-connection', MessageController.checkSessionConnected)
-routes.post('/api/start-session', MessageController.startSession)
-routes.post('/api/close-session', MessageController.closeSession)
+routes.get('/api/:session/show-all-sessions', MessageController.showAllSessions)
+routes.get('/api/:session/check-connection', MessageController.checkSessionConnected)
+routes.post('/api/:session/start-session', MessageController.startSession)
+routes.post('/api/:session/close-session', MessageController.closeSession)
 
 //SendMessages
-routes.post('/api/send-message', MessageController.sendMessage)
-routes.post('/api/send-image', MessageController.sendImage)
-routes.post('/api/send-file', MessageController.sendFile)
+routes.post('/api/:session/send-message', MessageController.sendMessage)
+routes.post('/api/:session/send-image', MessageController.sendImage)
+routes.post('/api/:session/send-file', MessageController.sendFile)
 
 // Group Functions
-routes.post('/api/create-group', MessageController.createGroup)
-routes.post('/api/join-code', MessageController.joinGroupByCode)
+routes.post('/api/:session/create-group', MessageController.createGroup)
+routes.post('/api/:session/join-code', MessageController.joinGroupByCode)
 
 // Device Functions
-routes.post('/api/change-username', MessageController.setProfileName)
-routes.post('/api/change-profile-image', MessageController.setProfileImage)
-routes.post('/api/close-session', MessageController.setProfileImage)
-routes.get('/api/show-all-contacts', MessageController.showAllContacts)
+routes.post('/api/:session/change-username', MessageController.setProfileName)
+routes.post('/api/:session/change-profile-image', MessageController.setProfileImage)
+routes.post('/api/:session/close-session', MessageController.setProfileImage)
+routes.get('/api/:session/show-all-contacts', MessageController.showAllContacts)
 
 module.exports = routes;
